34672bf1e5a8a4781c7fbfc2406dc71431ea769a
OlivPDL <pasdeloupolivier@gmail.com>
2023-11-15 18:55:09 +0100

    plotly use

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import matplotlib
 matplotlib.use('agg')  # Use the 'agg' backend
 import streamlit as st
+import plotly.express as px
 import requests
 import base64
 from datetime import datetime, timedelta
@@ -46,7 +47,8 @@ def get_weather_forecast(api_key, lat, lon):
         st.error(f"Error fetching weather data. Error code: {response.status_code}")
 
 def main():
-    st.title("Data Visualization PFE APP")
+    st.title("MVP PFE SEMHACH DASHBOARD")
+    st.markdown("<br>", unsafe_allow_html=True)
     #st.header("Electricity Exchange Prices in France")
     # Replace 'YOUR_CLIENT_ID' and 'YOUR_CLIENT_SECRET' with your actual values
     client_id = "1e0de31c-6c1a-4166-b0f8-d194bc163b13"
@@ -75,27 +77,21 @@ def main():
             start_dates = [entry['start_date'] for entry in spot_prices_data['france_power_exchanges'][0]['values']]
             prices = [entry['price'] for entry in spot_prices_data['france_power_exchanges'][0]['values']]
 
+    # Convert start_dates to datetime objects for better plotting
             start_dates = [datetime.strptime(date, "%Y-%m-%dT%H:%M:%S%z") for date in start_dates]
 
-             # Plot the data
-            fig, ax = plt.subplots()
-            ax.plot(start_dates, prices, label='Price')
-            ax.set_title('Electricity Exchange Prices in France')
-            ax.set_xlabel('Date and Time')
-            ax.set_ylabel('Price')
-            ax.legend()
-            
-            # Rotate x-axis labels
-            ax.set_xticklabels(ax.get_xticklabels(), rotation=45)
-            
-            plt.tight_layout()
+    # Create a Plotly figure
+            fig = px.line(x=start_dates, y=prices, labels={'x': 'Date and Time', 'y': 'Price'}, title='Electricity Exchange Prices in France')
 
-            # Display the Matplotlib plot in Streamlit
-            st.pyplot(fig)
+    # Customize the layout if needed
+            fig.update_layout(xaxis=dict(tickangle=45), margin=dict(l=0, r=0, t=89, b=0))
+
+    # Display the Plotly figure in Streamlit
+            st.plotly_chart(fig)
+            #on ajoute de l'espace entre les elements
+            st.markdown("<br>", unsafe_allow_html=True)
         else:
             st.error(f"Error fetching spot prices. Error code: {response.status_code}")
-    else:
-        st.error("Access token not found.")
     #st.header("Weather Forecast")
     # Replace 'YOUR_API_KEY', 46.17, and 20.52 with your OpenWeatherMap API key and desired coordinates
     api_key = '1a57c000e6a7972a8115a8e1aef41495'
@@ -104,13 +100,14 @@ def main():
 
     weather_df = get_weather_forecast(api_key, lat, lon)
 
-    fig_weather, ax_weather = plt.subplots()
-    ax_weather.plot(weather_df['Date and Time'], weather_df['Temperature (°C)'])
-    ax_weather.set_title('Weather Forecast for the Next 5 Days')
-    ax_weather.set_xlabel('Date and Time')
-    ax_weather.set_ylabel('Temperature (°C)')
-    plt.tight_layout()
-    st.pyplot(fig_weather)
+    
+# Create a Plotly figure for the weather forecast
+    fig_weather = px.line(weather_df, x='Date and Time', y='Temperature (°C)', labels={'Temperature (°C)': 'Temperature (°C)'}, title='Weather Forecast for the Next 5 Days')
 
+# Customize the layout if needed
+    fig_weather.update_layout(xaxis=dict(tickangle=45), margin=dict(l=0, r=0, t=50, b=0))
+
+# Display the Plotly figure in Streamlit
+    st.plotly_chart(fig_weather)
 if __name__ == "__main__":
     main()
