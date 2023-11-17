4a22e22dc23fed441254122b584172106141f08e
OlivPDL <pasdeloupolivier@gmail.com>
2023-11-17 15:55:37 +0100

    perm

--- setup.sh
+++ setup.sh
@@ -3,6 +3,7 @@
 # Créez un dossier pour les pilotes et copiez le fichier geckodriver
 mkdir drivers
 cp geckodriver drivers/
+chmod +x drivers/geckodriver
 
 # Installez les dépendances Python depuis le fichier requirements.txt
 pip install -r requirements.txt
